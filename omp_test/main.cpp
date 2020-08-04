650ceaf2721a3600bba2e8a0e30606c057f75cc8
Henrik Bartsch <bartsch@student01.bauv.unibw-muenchen.de>
2020-08-04 20:07:06 +0200

    prep for sup8

--- omp_test/main.cpp
+++ omp_test/main.cpp
@@ -100,7 +100,7 @@ void SetParameters(double& E1, double& E2, int& csteps, int& flagwarm,
                            0.826126871395416, 0.841369158110513,
                            0.851733020725652, 0.858342234203154,
                            0.862368243479785, 0.864741597831785};
-  int nn = 7;  // Matrix sent has the parameter nn=2!
+  int nn = 8;  // Matrix sent has the parameter nn=2!
   alpha = alpha_con[nn - 1];
   csteps = 1;
   ampface = 1;
@@ -724,7 +724,7 @@ int main(int argc, char* argv[]) {
 	double time1 = 0, time2 = 0, min1 = 0, min2 = 0;
 	vector<double> times1, times2, mins1, mins2;
 	Epetra_SerialDenseMatrix matrix1, matrix2;
-	string filePath = "sup7.dat";
+	string filePath = "sup8.dat";
 	matrix1.Shape(maxCache, maxThreads); matrix2.Shape(maxCache, maxThreads);
 	
 	std::cout << "Generating pre-work data." << endl;
@@ -734,22 +734,37 @@ int main(int argc, char* argv[]) {
 	vector<int> P = readinP("p_" + filePath);
 	std::cout << "P done." << endl;
 	int counter = 0, minPos = 0;
-	bool done = false;
 	
 	generateVars(filePath, matrix, b0);
 	
 	generateCounter(counter, P, matrix);
 	
 	// Counter for specific files:
-	// sup2: -1
-	// sup5: 0
-	// sup6:
-	// sup7:
-	// sup8: 
+	// sup2: -works-
+	// sup5: -works-
+	// sup6: -works-
+	// sup7: 513
+	// sup8: 2049
+	
+	// sup7 ONLY
+	// counter = 513;
+	
+	// sup8 ONLY
+	counter = 2049;
+	
+	/*
+	// Only for finding out counter-value's! Since other way doesnt work out ...
+	bool done = false;
 	
 	for (int i = 0; i < counter; i++){
-		if (P[i] > matrix.M()) { std::cout << "Something went wrong" << endl; }
+		if (P[i] > matrix.M()) { 
+			if (done == false){
+				done = true;
+				std::cout << "Something went wrong in line " + to_string(i) << endl;
+			}
+		}
 	}
+	*/
 	
 	std::cout << "Starting Static Runtime" << endl;
 	/*
