9115fecc646d407a41419f0af2adc8ed3609f54a
Nora Hagmeyer <nora.hagmeyer@unibw.de>
2020-09-23 07:47:46 +0200

    Makefiles for cluster job

---
+++ slurm_job.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -f
+#
+###############################
+# Specify your SLURM directives
+###############################
+#
+# Job name:
+#SBATCH --job-name bem 
+#SBATCH --output=slurm-%j-%x.out
+#SBATCH --error=slurm-%j-%x.err
+#
+# If you want to specify a certain number of nodes
+# and exactly 'ntasks-per-node' cpus on each node.
+##SBATCH --nodes=4
+##SBATCH --ntasks-per-node=24
+#
+# For hybrid mpi: e.g. 1 mpi process with
+# n openmp threads
+# SBATCH --ntasks=1
+# SBATCH --cpus-per-task=4
+# 
+# Allocate full node and block for other jobs
+#SBATCH --exclusive
+#
+# Walltime:
+# SBATCH --time=72:00:00
+###########################################
+
+# Set OMP specific environment
+
+module load openmpi/gcc/4.0.0
+
+echo "Loaded mpi"
+ 
+export OMP_NUM_THREADS=4
+
+srun /home/bartsch/BEM/bem #omp_test/test_program
