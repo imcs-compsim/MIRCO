7ced75c28942232b2cc29a8dd2e4ff479838a564
Tarik Sahin <tarik.sahin@unibw.de>
2021-06-04 10:12:31 +0200

    writing force directory generalized

--- main.cpp
+++ main.cpp
@@ -163,12 +163,13 @@ void writeForceToFile(Epetra_SerialDenseMatrix& y, string pathName, int& n){
   std::size_t botDirPos = pathName.find_last_of("/") + 1;
   // get directory
   std::string dir = pathName.substr(0, botDirPos);
-  // get file
-  std::string file = pathName.substr(botDirPos, pathName.length());
-  // cout << y << endl;
+  // get file name
+  std::string dat_file = pathName.substr(botDirPos, pathName.length());
+
+  std:string file = dir + "result_force_" + dat_file;
 
 	ofstream outfile;
-	outfile.open("result_force_" + file, std::ofstream::trunc);
+	outfile.open(file, std::ofstream::trunc);
 	
   for (i=0; i<n_size; i++){
     for (j=0; j<n_size; j++) {
