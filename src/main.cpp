a8354b9f22bf7929727525dfad58061ed5aa60bd
Rishav Shaw <rishav.shaw@unibw.de>
2022-02-11 15:10:37 +0100

    Add Teuchos test for exception for argc parameter #23

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
-#include <string>
 #include "evaluate.h"
+#include <Teuchos_TestForException.hpp>
+#include <string>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+  TEUCHOS_TEST_FOR_EXCEPTION(
+      argc != 2, std::invalid_argument,
+      "The code expects (only) an input file as argument");
   // reading the input file name from the command line
   std::string inputFileName = argv[1];
 
   double force = 0.0;
 
   Evaluate(inputFileName, force);
-
 }
