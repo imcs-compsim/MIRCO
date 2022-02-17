f1e579cfda51427c8fafeb0600ebfeda02a607df
Rishav Shaw <rishav.shaw@unibw.de>
2022-02-17 14:55:57 +0100

    Add function to compute residual #5

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -12,6 +12,7 @@
 #include <string>
 #include <vector>
 using namespace std;
+#include "computeresidual.h"
 #include "evaluate.h"
 #include "linearsolver.h"
 #include "matrixsetup.h"
@@ -190,22 +191,7 @@ void Evaluate(const std::string &inputFileName, double &force)
     // Compute residial
     // @{
     Epetra_SerialDenseMatrix res1;
-    if (A.M() != y.N())
-    {
-      std::runtime_error("Error 1: Matrix dimensions imcompatible");
-    }
-    res1.Shape(A.M(), A.M());
-
-    // res1=A*sol-b0(:,k)-wsol;
-#pragma omp parallel for schedule(static, 16)  // Always same workload -> Static
-    for (int x = 0; x < A.N(); x++)
-    {
-      for (int z = 0; z < y.M(); z++)
-      {
-        res1(x, 0) += A(x, z) * y(z, 0);
-      }
-      res1(x, 0) -= b0new(x, 0) + w(x, 0);  // [...]-b0(:,k) - wsol;
-    }
+    ComputeResidual(A, y, b0new, w, res1);
     // }
 
     // Compute number of contact node
