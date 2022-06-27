1d429206eb2fb31b33ac8a8468cb1f0a44111d75
Rishav Shaw <rishav.shaw@unibw.de>
2022-06-27 16:49:30 +0200

    Remove unnecessary comments

--- src/evaluate.h
+++ src/evaluate.h
@@ -6,9 +6,6 @@
 
 namespace MIRCO
 {
-  //   void Evaluate(double &force, double Delta, double lato, double delta, int resolution,
-  //       double Hurst, bool rand_seed_flag, std::string zfilePath, bool rmg_flag, bool rmg_seed,
-  //       double errf, double to1, int max_iter, double E, bool flagwarm, double k_el);
   void Evaluate(double &force, double Delta, double lato, double delta, double errf, double to1,
       int max_iter, double E, bool flagwarm, double k_el, Epetra_SerialDenseMatrix topology,
       double zmax, std::vector<double> x, Epetra_SerialDenseMatrix &y);
