7ffd81f64f3dd0b657980765d72758f7b8bfb70f
Hoang-Giang Bui <hgbk2008@gmail.com>
2024-11-04 17:37:08 +0100

    Add MIRCO namespace to UTILS::ChangeRelativePath

--- src/mirco_filesystem_utils.h
+++ src/mirco_filesystem_utils.h
@@ -3,18 +3,21 @@
 
 #include <string>
 
-namespace UTILS
+namespace MIRCO
 {
-  /*
-   * \brief Creates a file path relative to the current directory by concatenating
-   * 2 relative paths
-   *
-   * This function takes a sourcefilename to a file relative to the current
-   * directory, and a targetfilename, that is relative to the sourcefilepath and
-   * concatenates them. If the targetfilename is already an absolute path, it will
-   * do nothing.
-   */
-  void ChangeRelativePath(std::string& targetfilename, const std::string& sourcefilename);
-}  // namespace UTILS
+  namespace UTILS
+  {
+    /*
+     * \brief Creates a file path relative to the current directory by concatenating
+     * 2 relative paths
+     *
+     * This function takes a sourcefilename to a file relative to the current
+     * directory, and a targetfilename, that is relative to the sourcefilepath and
+     * concatenates them. If the targetfilename is already an absolute path, it will
+     * do nothing.
+     */
+    void ChangeRelativePath(std::string& targetfilename, const std::string& sourcefilename);
+  }  // namespace UTILS
+}  // namespace MIRCO
 
 #endif /* SRC_FILESYSTEM_UTILS_H_ */
